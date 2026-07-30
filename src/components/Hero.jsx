57e2bb965965c1d14d391eb9535ecf50b749b6a8
charithkale <charithkale@gmail.com>
2026-07-30 15:26:46 +0530

    icon modify

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,9 +29,9 @@ export default function Hero() {
           </div>
           <div className="social-row">
             <a href="https://github.com" target="_blank" rel="noreferrer" aria-label="GitHub">
-            <img src="https://img.icons8.com/3d-fluency/94/github-logo.png" alt="github-logo" className="icon"/>       </a>
+            <img src="/github-white-icon.png" alt="github-logo" className="icon"/>       </a>
             <a href="https://www.linkedin.com/in/charith-kale" target="_blank" rel="noreferrer" aria-label="LinkedIn">
-           <img  src="https://img.icons8.com/color/48/linkedin.png" alt="linkedin" className="icon"/>
+           <img  src="/linkedin-icon.png" alt="linkedin" className="icon"/>
             </a>
           </div>
         </div>
